906b3917a71ac2587ac6356a2f6faac06a4edfe8
ndk1195 <ndkhoa1195@gmail.com>
2024-12-01 19:47:56 +0700

    add one box

--- src/App.jsx
+++ src/App.jsx
@@ -60,7 +60,39 @@ function App() {
             </div>
           </div>
         </Box>
-        <Box></Box>
+        <Box>
+          <div className="mb-8 mt-8 flex flex-col items-center lg:mb-9 lg:mt-12 lg:flex-row">
+            <div className="mb-6 text-center lg:mb-0 lg:mr-11 lg:text-left">
+              <h3 className="mb-2 text-[32px] font-bold leading-none text-black">
+                $89,914
+              </h3>
+              <p className="text-sm leading-none text-dark-gray lg:text-[15px]">
+                of $100,000 backed
+              </p>
+            </div>
+            <div className="mb-6 h-[1px] w-20 bg-black bg-opacity-15 lg:mb-0 lg:mr-12 lg:h-[63px] lg:w-[1px]"></div>
+            <div className="mb-6 text-center lg:mb-0 lg:mr-[91px] lg:text-left">
+              <h3 className="mb-2 text-[32px] font-bold leading-none text-black">
+                5,007
+              </h3>
+              <p className="text-sm leading-none text-dark-gray lg:text-[15px]">
+                total backers
+              </p>
+            </div>
+            <div className="mb-6 h-[1px] w-20 bg-black bg-opacity-15 lg:mb-0 lg:mr-12 lg:h-[63px] lg:w-[1px]"></div>
+            <div className="mb-6 text-center lg:mb-0 lg:mr-11 lg:text-left">
+              <h3 className="mb-2 text-[32px] font-bold leading-none text-black">
+                56
+              </h3>
+              <p className="text-sm leading-none text-dark-gray lg:text-[15px]">
+                days left
+              </p>
+            </div>
+          </div>
+          <div className="relative h-3 w-full rounded-[33.5px] bg-[#2f2f2f] bg-opacity-5">
+            <div className="absolute h-full w-1/2 rounded-[33.5px] bg-moderate-cyan"></div>
+          </div>
+        </Box>
         <Box></Box>
       </section>
       {/* main */}
